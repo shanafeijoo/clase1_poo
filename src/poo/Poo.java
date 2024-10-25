c8a2af85c6dbc0291d648d18b701e903562d3b32
Lab5 <Lab5@LAB1-DOCENTE>
2024-10-25 19:05:55 -0300

    hola mundo

--- src/poo/Poo.java
+++ src/poo/Poo.java
@@ -15,6 +15,8 @@ public class Poo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("hola mundo");
     }
     
 }
